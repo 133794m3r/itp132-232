825c914a9c04920d164a4a707c64c927f781abe9
Macarthur Inbody <133794m3r@gmail.com>
2020-04-27 12:38:01 -0400

    added matrix comments

--- itp132/matrix_functions.cpp
+++ itp132/matrix_functions.cpp
@@ -7,26 +7,48 @@
 typdef unsigned int uint32;
 
 
-
-
 //the main function
 int main() {
-  int a[4]={0,0,0,0};
-  int b[4]={1,2,3,4};
-  int *r = new int[4];
-  
-  float c[4]={2,3,4,5};
-  float d[4]={0.1,0.1,0.1,0.1};
-  float *r2=new float[4];
-  
-  r=matrix_add(a,b);
-  r2=matrix_add(c,d);
-  int i=0;
-  for(i=0;i<4;i++){
-    std::cout << r[i] << std::endl;
-  }
-  
-  for(i=0;i<4;i++){
-    std::cout << r2[i] << std::endl;
-  }
+	int i=0;
+	//the int arrays.
+	int a[4]={0,0,0,0};
+	int b[4]={1,2,3,4};
+	//resulting heap object.
+	int *r = new int[4];
+	
+	//float version.
+	float c[4]={2,3,4,5};
+	float d[4]={0.1,0.1,0.1,0.1};
+	//the result.
+	float *r2=new float[4];
+	
+	//get result of ints.
+	r=matrix_add(a,b);
+	//get result of floats.
+	r2=matrix_add(c,d);
+	
+	//output the results.
+	std::cout << "Int Example: [";
+	for(i=0;i<4;i++){
+		//if we're on the second value+ add a comma.
+		if(i > 0)
+			std::cout << ",";
+		
+		std::cout << r[i];
+	}
+	std::cout << "]" << std::endl;
+	
+	
+	//do the float version.
+	std::cout << "Float Example: [";	
+	for(i=0;i<4;i++){
+		if(i > 0)
+			std::cout << ",";
+		
+		std::cout << r2[i] << std::endl;
+	}
+	std::cout << "]" << std::endl;
+	
+	//always return zero.
+	return 0;
 }
--- itp132/matrix_functions.hxx
+++ itp132/matrix_functions.hxx
@@ -2,19 +2,32 @@
 #define MATRIX_FUNC
 //this is only just for a 2x2 matrix.
 //this is the non-operator overloaded version showing templates.
+
+/*
+* matrix_add
+* Templated function that takes any type and adds the values together
+*then it returns the result as a new 4 value array.
+*/
 template <typename T> T *matrix_add(T *a, T *b){
 	uint32 i=0;
+	//create a new pointer to an array of the type specified.
 	T *c=new T[4];
+	//add them all together.
 	c[0]=a[0]+b[0];
 	c[1]=a[1]+b[1];
 	c[2]=a[2]+b[2];
 	c[3]=a[3]+b[3];
-
+	//return the value.
 	return c;
 }
-
+/*
+* matrix_sub
+* Templated subtraction function. Operates on a 4 value matrix.
+* This will return a new set of values.
+*/
 template <typename T> T *matrix_sub(T *a, T *b){
 	uint32 i=0;
+	//return the resulting heap.
 	T *c=new T[4];
 	c[0]=a[0]-b[0];
 	c[1]=a[1]-b[1];
