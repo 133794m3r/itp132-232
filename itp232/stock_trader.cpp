fabe468e67528b2f01481bcaf8a8f78aaec71f65
Macarthur Inbody <133794m3r@gmail.com>
2020-05-03 21:07:58 -0400

    stock trader is finally getting cleaned up. The program finally does some basic purchasing now. Have to add selling then it'll be done.

--- itp232/stock_trader.cpp
+++ itp232/stock_trader.cpp
@@ -1,37 +1,84 @@
 #include <iostream>
-#include <vector>
-#include "accounts.h"
+#include "terminal.h"
+#include "stock_trader.h"
+#include <stdio.h>
 //this'll be a simple trading game. Buy an item for a price, then see if you want to seell it. 
 //costs'll be randomized each day. Goal is to have the maximum amount of money in the end.
-typedef std::vector<Item *> vector_ptr_item;
-typedef std::vector<int *> vector_ptr_int;
-void update_prices(void){
-	
-}
-void update_amounts(void){
-	
-}
-void print_items(vector_ptr_item &items){
+
+
+typedef std::vector<Item*> vector_ptr_item;
+typedef std::vector<int*> vector_ptr_int;
+
+void print_items(vector_ptr_item &Items){
 	int i=0;
-	
-	for(Item *current:items){
-		if(i>0)
-			std::cout << ",";
+		std::cout.precision(2);
+		std::cout.setf( std::ios::fixed, std:: ios::floatfield );
+	for(Item *current:Items){
+		printf("\x1b[%dm%d\x1b[0m:",1,i++);
 		std::cout << current->get_name();
+
+		std::cout << " $" << current->get_price();
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
+	//std::cout << std::endl;
+}
+
+void update_prices(vector_ptr_item &Items){
+	double current_price=0.0f;
+	double new_price=0.0f;
+	for(Item *current_item:Items){
+		current_price=current_item->get_price();
+		new_price=to_fixed(current_price*xorshift128(0.85,1.15),2);
+		current_item->set_price(new_price);
+	}
+}
+
+int purchase_item_menu(){
+	return 0;
 }
 int main(){
+	//xor128 isn't the best PRNG but it's good enough for this simple exercise.
+	s_xor_128();
 	std::string username;
+	unsigned int current_day=0;
 	double starting_balance=1000.0;
+	int selection=0;
 	vector_ptr_item items = { new Item("Tuna Fish", 10.5), new Item("Bunch of Bananas", 3.7), new Item("Barrel of Oil", 100.35),new Item("Wuhan Bat Soup",66.6)};
+	int tx_success=0;
 	std::cout << "Please enter your name: ";
 	getline(std::cin, username);
 	Account *user = new Account(username,starting_balance);
-	Inventory *user_inventory=new Inventory(items.size());
-	std::cout << "Welcome to the Trading simulator. You will play for 3 days. The goals is to end up with the maximum money at the end of it.\r\nYou start off with $1000.0\r\n Each day there will be a list of items to buy at the start, then a list of items that people want to buy. Both will have a price listed beside them along with how many of them there are. At the end of the game any unsold items will be automatically sold for the final sale price and added to your final total" << std::endl;
-	std::cout << user->get_name() << " " << user->get_balance() << std::endl;
+	Inventory *user_inventory=new Inventory(items.size(),items);
+	std::cout << "\x1b[0;0H";
+	std::cout << "\x1b[0J" << "\x1b[0;0H";
+	std::cout << "Welcome to the Trading simulator. You will play for 3 days. The goals is to end up with the maximum money at the end of it.\r\nYou start off with $1000.0\r\nEach day there will be a list of items to buy at the start, then a list of items that people want to buy. Both will have a price listed beside them along with how many of them there are. At the end of the game any unsold items will be automatically sold for the final sale price and added to your final total" << std::endl << std::endl;
 	
-	print_items(items);
+	
+	for(current_day=0;current_day < 2;current_day++){
+		selection=0;
+		if(current_day > 0)
+			std::cout << "\x1b[10F\x1b[0J";
+		while(selection != -1){
+			std::cout << "Welcome to Day " << current_day << ", \x1b[1;3m" << username << "\x1b[0m!\r\nHere are the items you can buy: ";
+			std::cout << "Select the item you want to buy pressing the respective number. To quit buying for the day enter -1." << std::endl << std::endl;
+			print_items(items);
+			std::cout << "\x1b[1mSelection:";
+			std::cin >> selection;
+			std::cout << "\x1b[0m";
+			if(selection == -1)
+				break;
+			tx_success=user_inventory->purchase_item(*user,*items[selection]);
+			std::cout << tx_success << std::endl;
+			if(tx_success == 1){
+				std::cout << "You have successfully purchased your items." << std::endl;
+			}
+			else if(tx_success == -1){
+				std::cout << "You tried to buy more than you could afford!" << std::endl;
+			}
+			std::cout << "\x1b[5F\x1b[0J";				
+		}
+		update_prices(items);
+		system("pause");
+	}
 	return 0;
 }
--- itp232/stock_trader.h
+++ itp232/stock_trader.h
@@ -1,19 +1,37 @@
 #ifndef _BANK_H_
 #define _BANK_H_
+#include <cmath>
+#include <vector>
+#include <memory>
+#include <chrono>
+#include "random.h"
+//template <typename T> T rand_range_real(T L, T H){ return L+((H-L)*(xorshift128()/UINT64_MAX));}
+//template <typename T> T rand_range(T L, T H){return rand_range_real(L,H);}
+
+double rand_double(double L,double H){return L+(rand()/(RAND_MAX /(H-L)));}
+
+template <typename F,typename P> F to_fixed(F num,P precision){P modifier=pow(10,precision); return round(num*modifier)/modifier;}
+
+typedef unsigned int uint32;
+
 class Item{
 	private:
 		std::string name;
-		double cost;
+		double price;
+		unsigned int id=0;
+		static unsigned int current_id;
 	public:
+
 		Item(std::string n="None",double c=0.0){
 			name=n;
-			cost=c;
+			price=c;
+			id=current_id++;
 		}
-		double get_cost(){
-			return cost;
+		double get_price(){
+			return price;
 		}
-		void set_cost(double c){
-			cost=c;
+		void set_price(double c){
+			price=c;
 		}
 		std::string get_name(){
 			return name;
@@ -21,7 +39,49 @@ class Item{
 		void set_name(std::string n){
 			name=n;
 		}
+		unsigned int get_id(){
+			return id;
+		}
+		
 	
+};
+unsigned int Item::current_id=0;
+class Account;
+class Inventory{
+	private:
+		std::vector<int> total_owned;
+		std::vector<Item*> Items;
+	public:
+		Inventory(int total_items,std::vector<Item *>itms){
+			int i=0;
+			for(i=0;i<total_items;i++){
+				Items.push_back(itms[i]);
+				total_owned.push_back(0);
+			}
+		}
+		
+		void add_item(uint32 itm_idx, uint32 amount){
+			total_owned[itm_idx]+=amount;
+		}
+		
+		bool remove_item(uint32 itm_idx, uint32 amount){
+			if(total_owned[itm_idx] > amount){
+				total_owned[itm_idx]-=amount;
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		int purchase_item(Account &usr,Item &itm);
+		int sell_item(Account &usr, Item &itm, int amount);
+		void print_items(){
+			for(Item *item:Items){
+				std::cout << "name: " << item->get_name() << " ";
+				std::cout << "cost: " << item->get_price() << " ";
+				std::cout << "id: " << item->get_id() << " " << std::endl;
+			}
+		}
 };
 class Account{
 	private:
@@ -57,27 +117,43 @@ class Account{
 				return false; 
 			}
 		}
-		friend purchase_item(Item &itm, int amount);
-		friend sell_item(Item &itm, int amount);
+		friend int Inventory::purchase_item(Account &usr,Item &itm);
+		friend int Inventory::sell_item(Account &usr,Item &itm, int amount);
 };
-class Inventory{
-	private:
-		std::vector<int> totals;
-	public:
-		Inventory(int total_items){
-			int i=0;
-			while(i>0){
-				totals.push_back(0);
+	int Inventory::purchase_item(Account &usr,Item &itm){
+			int amount=0;
+			double available_funds = usr.get_balance();
+			double total_cost = 0;
+			double price=itm.get_price();
+			int can_buy = available_funds / price;
+			std::cout << "\x1b[9A\x1b[0J";
+			std::cout << "To exit enter zero as the amount.\r\nYou have selected " << itm.get_name() << std::endl;
+			std::cout << "You can buy: " << can_buy <<".";
+			std::cout << "How many would you like to buy?";
+			std::cin >> amount;
+			if(amount ==0)
+				return 0;
+			total_cost=amount*price;
+	
+			if(total_cost <= available_funds){
+				total_owned[itm.get_id()]+=amount;
+				usr.balance-=total_cost;
+				return 1;
 			}
-		}
-		void purchase_item(Item &itm){
-			
-		}
-		void add_item(Item &itm){
-			
-		}
-		void add_items(int item_index){
-			totals[item_index]++;
-		}
-};
+			else
+				return -1;
+	}
+		
+	int Inventory::sell_item(Account &usr, Item &itm, int amount){
+			unsigned int owned=total_owned[itm.get_id()];
+			double price = itm.get_price();
+			if(owned >= amount){
+				usr.balance+=(amount*price);
+				total_owned[itm.get_id()]-=amount;
+				return true;
+			}
+			else{
+				return false;
+			}
+	}
 #endif
