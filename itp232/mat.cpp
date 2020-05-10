fa1bcd7eff44c4d0cd80354d3eb0d1d6de1c11d5
Macarthur Inbody <133794m3r@gmail.com>
2020-05-10 00:34:42 -0400

    Finally have determinants working.

--- itp232/mat.cpp
+++ itp232/mat.cpp
@@ -1,83 +1,98 @@
 #include <iostream>
 #include "matrix.hxx"
 int main() {
-int row=2;
-int col=2;
-//Matrix mat1(2,2,1);
-//std::cout << "mat1" << std::endl;
-  //Matrix<int> result(row,col,0);
-  int i=0;
-  int j=0;
-  int k=0;
-  matrix_int mat1(col,row,0);
-  for(i=0;i<row;i++){
-    for(j=0;j<col;j++){
-      mat1(i,j)=(k++);
-    }
-  }
+	int row=2;
+	int col=2;
+	//Matrix mat1(2,2,1);
+	//std::cout << "mat1" << std::endl;
+	//Matrix<int> result(row,col,0);
+	int i=0;
+	int j=0;
+	int k=0;
+	matrix_int mat1(col,row,0);
 
-  std::cout << "test_mat=[";
-  for(i=0;i<row;i++){
-    std::cout << "[";
-    for(j=0;j<col;j++){
-      std::cout << mat1(i,j);
-	  if(j < col-1)
-		  std::cout << ",";
-    }
-    std::cout << "]";
-	if(i < row-1)
-		std::cout << ",";	
-  }
-  std::cout << "];" << std::endl;
-  row=2;
-  col=2;
-  //Matrix mat2(col,row,5);
-  //Matrix result(2,2,0);
-matrix_int mat2(col,row,5);
-matrix_int result(2,2,0);
-  std::cout << "mat2=[";
-  for(i=0;i<col;i++){
-    std::cout << "[";
-    for(j=0;j<row;j++){
+	for(i=0;i<row;i++){
+		for(j=0;j<col;j++){
+			mat1(i,j)=(k++);
+		}
+	}
 
-       std::cout << mat2(i,j);
-	  if(j < row-1)
-		  std::cout << ",";
-    }
-    std::cout << "]";
-	if(i < col-1)
+	std::cout << "test_mat=[";
+	for(i=0;i<row;i++){
+		std::cout << "[";
+		for(j=0;j<col;j++){
+			std::cout << mat1(i,j);
+			if(j < col-1)
+				std::cout << ",";
+		}
+		std::cout << "]";
+		if(i < row-1)
+			std::cout << ",";	
+	}
+	std::cout << "];" << std::endl;
+	
+	row=2;
+	col=2;
+	//Matrix mat2(col,row,5);
+	//Matrix result(2,2,0);
+	matrix_int mat2(col,row,5);
+	matrix_int result(2,2,0);
+
+	std::cout << "mat2=[";
+	for(i=0;i<col;i++){
+		std::cout << "[";
+		for(j=0;j<row;j++){
+			std::cout << mat2(i,j);
+			if(j < row-1)
+				std::cout << ",";
+		}
+		std::cout << "]";
+		if(i < col-1)
+			std::cout << ",";	
+	}
+	std::cout << "];" << std::endl;	
+	
+	result=mat1-mat2;
+	std::cout << "result=[";
+	for(i=0;i<row;i++){
+		std::cout << "[";
+		for(j=0;j<col;j++){
+			std::cout << result(i,j);
+			if(j < col-1)
+				std::cout << ",";
+	}
+		std::cout << "]";
+
+		if(i < row-1)
 		std::cout << ",";	
-  }
-  std::cout << "];" << std::endl;  
-  
-  result=mat1-mat2;
-  std::cout << "result=[";
-  for(i=0;i<row;i++){
-    std::cout << "[";
-    for(j=0;j<col;j++){
-      std::cout << result(i,j);
-	  if(j < col-1)
-		  std::cout << ",";
-    }
-    std::cout << "]";
-	if(i < row-1)
-		std::cout << ",";	
-  }
-  std::cout << "];" << std::endl;
-  result=result+10;
-  std::cout << "result=[";
-  for(i=0;i<row;i++){
-    std::cout << "[";
-    for(j=0;j<col;j++){
-      std::cout << result(i,j);
-	  if(j < col-1)
-		  std::cout << ",";
-    }
-    std::cout << "]";
-	if(i < row-1)
-		std::cout << ",";	
-  }
-  std::cout << "];" << std::endl;  
-  std::cout << result << std::endl;
-  return 0;
+	}
+	std::cout << "];" << std::endl;
+	
+	result=result+10;
+	std::cout << "result=[";
+	for(i=0;i<row;i++){
+		std::cout << "[";
+		for(j=0;j<col;j++){
+			std::cout << result(i,j);
+			if(j < col-1)
+				std::cout << ",";
+	}
+		std::cout << "]";
+		if(i < row-1)
+			std::cout << ",";	
+	}
+	std::cout << "];" << std::endl;	
+	std::cout << result << std::endl;
+	
+	Matrix<double> det_mat(2,2,0);
+	k=1;
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			det_mat(i,j)=(k+=2);
+		}
+	}
+	std::cout << "det_matrix" << std::endl << det_mat << std::endl;
+	int the_determinant=det(det_mat);
+	std::cout << the_determinant << std::endl;
+	return 0;
 }
--- itp232/matrix.hxx
+++ itp232/matrix.hxx
@@ -1,8 +1,12 @@
 #ifndef MATRIX_CLASS_DEMO
 #define MATRIX_CLASS_DEMO
 #include <vector>
-template <typename T> class Matrix;
+#include <valarray>
+#include "math.h"
+template <class T> class Matrix;
 template <typename T> std::ostream& operator<<( std::ostream&, const Matrix<T>& );
+//template <typename T>T det(Matrix<T>&);
+//template <typename scalar_type> bool lud(Matrix<scalar_type> &A,scalar_type &determinant);
 template <class T> class Matrix{
 	private:
 		unsigned long rows;
@@ -21,12 +25,17 @@ template <class T> class Matrix{
 				array.push_back(initial_value);				
 			}
 		}
-		
-		T get_cols(){
+		unsigned long get_cols() {
+		    return this->cols;
+		}
+		unsigned long get_cols() const{
 			return this->cols;
 		}
-		T get_rows(){
+		unsigned long get_rows() const{
 			return this->rows;
+		}
+		unsigned long get_rows() {
+		    return this->rows;
 		}
 		
 		size_t index(unsigned long x, unsigned long y) const{
@@ -40,14 +49,19 @@ template <class T> class Matrix{
 		const T &operator()(const unsigned long x, const unsigned long y) const{
 			return array[x+(rows*y)];
 		}
-		
+		T &operator[](const size_t x){
+		    return this->array[x];
+		}
+		const T &operator[](const size_t x) const{
+		    return this->array[x];
+		}
 		T *get_elem(T x,T y){
 			T *el;
 			el=&array[x+(rows*y)];
 			return el;
 		}
-		
-		void invalid_dim(std::string op, const Matrix &other_matrix){
+
+		void invalid_dim(const std::string& op, const Matrix &other_matrix){
 			throw std::invalid_argument("For " + op + " Matrix 1's columns and rows must match Matrix 2's columns and rows.\r\nM1.cols="+std::to_string(cols)+" M1.rows="+std::to_string(rows)+" M2.cols="+std::to_string(other_matrix.rows)+" M2.rows="+std::to_string(other_matrix.cols)+"\r\n");
 		}
 		
@@ -176,7 +190,10 @@ template <class T> class Matrix{
 	*/
 // Output stream function for matrix
 //friend std::ostream& operator<<( std::ostream&, const Matrix<T>& );
-friend std::ostream& operator<< <T>( std::ostream&, const Matrix<T>& );
+	friend std::ostream& operator<< <T>( std::ostream &, const Matrix<T> &);
+
+    //friend T gaussian_reduction <T>(const Matrix<T> &);
+	template <class U> friend bool lud(Matrix<T> &,T &determinant);
 	~Matrix();
 		
 };
@@ -188,10 +205,71 @@ template <typename T> std::ostream& operator<<(std::ostream& os, const Matrix<T>
 	//int w = os.width();
 	unsigned long rows=m.rows;
 	unsigned long cols=m.cols;
-    for (unsigned long i=0; i < rows; i++)
-       for (unsigned long j=0; j < cols; j++)
-          os << m(i,j) << (j == cols-1 ? '\n' : '\t');
-   return os;
+	for (unsigned long i=0; i < rows; i++)
+		 for (unsigned long j=0; j < cols; j++)
+			os << m(i,j) << (j == cols-1 ? '\n' : '\t');
+	 return os;
+}
+
+
+template <typename scalar_type> bool lud(Matrix<scalar_type> &A,double &determinant) {
+    size_t i, j, k, l, i_max;
+    //size_t i,j,k;
+    std::vector<scalar_type> tmp_row;
+    //double max_matrix,absolute_matrix;
+    double max_matrix, absolute_matrix;
+    size_t n = A.get_cols();
+    size_t i_pos = 0;
+    size_t j_pos = 0;
+    std::vector<scalar_type> P;
+
+    determinant = scalar_type(1);
+    for (i = 0; i < n; i++) {
+        P.push_back(i);
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        max_matrix = abs(A[P[i] * n + i]);
+        i_max = i;
+        for (j = i + 1; j < n; j++) {
+            if ((absolute_matrix = abs(A[j] * n + i)) > max_matrix) {
+                max_matrix = absolute_matrix;
+                i_max = j;
+            }
+        }
+        if (i_max != i) {
+            std::swap(P[i], P[i_max]);
+            determinant = -determinant;
+        }
+
+        i_pos = P[i] * n;
+        if (abs(A[i_pos + i]) < epsilon(A[i_pos + i])) {
+            return false;
+        }
+        determinant *= A[i_pos + i];
+        for (j = i + 1; j < n; j++) {
+            size_t j_pos = P[j] * n;
+            scalar_type a = A[j_pos + i] /= A[i_pos + i];
+
+            for (k = i + 1; k < n; k++) {
+                A[j_pos + k] -= a * A[i_pos + k];
+            }
+        }
+    }
+    determinant*= A[P[i]*n+i];
+    return true;
+}
+
+template <typename T> double det(const Matrix<T> &mat){
+    if(mat.get_cols() != mat.get_rows()){
+        throw std::invalid_argument( "det(Matrix<T> A): Cannot calculate the determinant of a non-square matrix!");
+    }
+    Matrix<double> tmp_matrix=mat;
+    double determinant=0;
+    if(!lud(tmp_matrix,determinant))
+    determinant=0;
+
+    return determinant;
 }
 
 typedef Matrix<int> matrix_int;
