03243507c7c0c4ee319f1eaafb63608846b26f00
Macarthur Inbody <133794m3r@gmail.com>
2020-05-26 19:33:12 -0400

    finally actually have cmake setup properly

---
+++ itp132/CMakeFiles/array_sort_search.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/array_sort_search.dir/array_sort_search.cpp.o"
+  "array_sort_search"
+  "array_sort_search.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/array_sort_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ itp132/CMakeFiles/matrix_functions.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_functions.dir/matrix_functions.cpp.o"
+  "matrix_functions"
+  "matrix_functions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_functions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
